6f3a9260e15ff1c1c1017a1b27c3fd51e6f4efb9
User User <useruser1985official@outlook.com>
2024-11-11 17:04:37 -0300

    Adição de Novos Scripts

---
+++ atualizacao-apt.sh
@@ -0,0 +1,13 @@
+apt update
+
+apt dist-upgrade -y
+
+apt clean
+
+apt autoclean
+
+apt autoremove --purge -y
+
+rm ~/.bash_history
+
+exit
---
+++ instalar-programas-termux.sh
@@ -0,0 +1,129 @@
+apt install gcc -y
+
+apt install g++ -y
+
+apt install gdb -y
+
+apt install make -y
+
+apt install openjdk -y
+
+apt install mysql-server -y
+
+apt install node -y
+
+apt install gobjc -y
+
+apt install gnustep-devel -y
+
+apt install php -y
+
+apt install apache2 -y
+
+apt install lamp-server^ -y
+
+apt install python -y
+
+apt install build-essential -y
+
+apt install libgl1-mesa-dev -y
+
+apt install perl -y
+
+apt install ruby-full -y
+
+apt install nasm -y
+
+apt install postgresql -y
+
+apt install psql -y
+
+apt install sqlite -y
+
+apt install gnupg -y
+
+apt install tor -y
+
+apt install proxychains -y
+
+apt install md5sum -y
+
+apt install sha1sum -y
+
+apt install sha256sum -y
+
+apt install steghide -y
+
+apt install dmitry -y
+
+apt install macchanger -y
+
+apt install openssh-client -y
+
+apt install openssh-server -y
+
+apt install nmap -y
+
+apt install ncat -y
+
+apt install nping -y
+
+apt install tcpdump -y
+
+apt install bind9 -y
+
+apt install ping -y
+
+apt install traceroute -y
+
+apt install iproute2 -y
+
+apt install net-tools -y
+
+apt install tshark -y
+
+apt install aircrack-ng -y
+
+apt install bully -y
+
+apt install whois -y
+
+apt install nikto -y
+
+apt install dig -y
+
+apt install sqlmap -y
+
+apt install hydra -y
+
+apt install john -y
+
+apt install hashcat -y
+
+apt install netcat -y
+
+apt install dirb -y
+
+apt install crunch -y
+
+apt install medusa -y
+
+apt install patator -y
+
+apt install ettercap-text-only -y
+
+apt install vim -y
+
+apt install nano -y
+
+apt install whiptail -y
+
+apt install dialog -y
+
+apt install ltrace -y
+
+apt install strace -y
+
+apt install objdump -y
+
+apt install ht -y
