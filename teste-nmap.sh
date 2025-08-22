e52ccfecf9f9e805fa6ff4c677f9e1cca3b0e73a
User User <useruser1985official@outlook.com>
2025-08-22 20:44:37 -0300

    Atualização Semanal

--- teste-nmap.sh
+++ teste-nmap.sh
@@ -12,9 +12,9 @@ fi
 sudo nmap -T4 -A -v 65.181.111.13
 
 # F:
-sudo nmap -T4 -A -v 147.79.105.138
+sudo nmap -T4 -A -v 77.37.42.227
 
-sudo nmap -T4 -A -v 77.37.42.99
+sudo nmap -T4 -A -v 147.79.105.35
 
 sudo nmap -T4 -A -v 147.93.14.189
 
@@ -24,6 +24,11 @@ sudo nmap -T4 -A -v 34.120.251.119
 sudo nmap -T4 -A -v 172.67.221.192
 
 sudo nmap -T4 -A -v 104.21.86.163
+
+# A:
+sudo nmap -T4 -A -v 191.252.101.243
+
+sudo nmap -T4 -A -v 34.120.251.119
 
 clear
 
